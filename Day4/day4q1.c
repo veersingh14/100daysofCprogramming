b355318740fdbed701a7831769bd3d151004412b
veersingh14 <veersingh4178@gmail.com>
2026-08-13 17:18:31 +0000

    day 4 q1

---
+++ Day4/day4q1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+
+    printf("Enter a: ");
+    scanf("%d", &a);
+
+    printf("Enter b: ");
+    scanf("%d", &b);
+
+    a = a + b;
+    b = a - b;
+    a = a - b;
+
+    printf("After swapping:\n");
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+
+    return 0;
+}
