882e199be5365a6c78165260588424f6541e3a9a
veersingh14 <veersingh4178@gmail.com>
2026-08-17 11:04:19 +0000

    day8 q2

---
+++ day8/day8q2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a >= b && a >= c) {
+        printf("Largest is %d", a);
+    }
+    else if (b >= a && b >= c) {
+        printf("Largest is %d", b);
+    }
+    else {
+        printf("Largest is %d", c);
+    }
+
+    return 0;
+}
