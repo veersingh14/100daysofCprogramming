a709a25b10b3ccfb215f664f713cefb00e3bdc16
veersingh14 <veersingh4178@gmail.com>
2026-08-22 18:09:09 +0000

    day13 q1

---
+++ day13/day13q1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    char op;
+
+    scanf("%d %d %c", &a, &b, &op);
+
+    switch (op) {
+        case '+':
+            printf("%d", a + b);
+            break;
+
+        case '-':
+            printf("%d", a - b);
+            break;
+
+        case '*':
+            printf("%d", a * b);
+            break;
+
+        case '/':
+            printf("%d", a / b);
+            break;
+
+        case '%':
+            printf("%d", a % b);
+            break;
+
+        default:
+            printf("Invalid operator");
+    }
+
+    return 0;
+}
