0a4e5671178dbed1e93c8c71aab9b320e58811ac
veersingh14 <veersingh4178@gmail.com>
2026-08-29 04:57:57 +0000

    day19 q1

---
+++ day19/day19q1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, lcm;
+
+    scanf("%d %d", &a, &b);
+
+    if (a > b)
+        lcm = a;
+    else
+        lcm = b;
+
+    while (1)
+    {
+        if (lcm % a == 0 && lcm % b == 0)
+        {
+            break;
+        }
+
+        lcm++;
+    }
+
+    printf("%d", lcm);
+
+    return 0;
+}
