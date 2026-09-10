b1ecc84959b32e576542efb14cad8fd1662153c3
veersingh14 <veersingh4178@gmail.com>
2026-09-10 05:12:32 +0000

    day26 q2

---
+++ day026/day26q2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+
+    // 1 star
+    printf("*\n\n");
+
+    // 3 stars
+    for (i = 1; i <= 3; i++)
+    {
+        printf("*\n");
+    }
+    printf("\n");
+
+    // 5 stars
+    for (i = 1; i <= 5; i++)
+    {
+        printf("*\n");
+    }
+    printf("\n");
+
+    // 3 stars
+    for (i = 1; i <= 3; i++)
+    {
+        printf("*\n");
+    }
+    printf("\n");
+
+    // 1 star
+    printf("*\n");
+
+    return 0;
+}
