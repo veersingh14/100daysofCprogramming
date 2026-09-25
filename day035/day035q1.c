946ce14f821a3bd62778857ebb739d57a309e9d1
veersingh14 <veersingh4178@gmail.com>
2026-09-25 16:31:42 +0000

    day035 q1

---
+++ day035/day035q1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[100], n, i;
+    int largest, second;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter array elements: ");
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    largest = a[0];
+    second = a[0];
+
+    for (i = 1; i < n; i++)
+    {
+        if (a[i] > largest)
+        {
+            second = largest;
+            largest = a[i];
+        }
+        else if (a[i] > second && a[i] != largest)
+        {
+            second = a[i];
+        }
+    }
+
+    printf("Second largest element = %d", second);
+
+    return 0;
+}
