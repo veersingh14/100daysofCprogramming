ec072c820b3ac91280f9fac11c03bb526eade39b
veersingh14 <veersingh4178@gmail.com>
2026-09-25 16:42:20 +0000

    day036 q2

---
+++ day036/day036q2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[10][10], rows, cols, i, j, sum = 0;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+
+    printf("Enter matrix elements:\n");
+
+    for (i = 0; i < rows; i++)
+    {
+        for (j = 0; j < cols; j++)
+        {
+            scanf("%d", &a[i][j]);
+            sum = sum + a[i][j];
+        }
+    }
+
+    printf("Sum of all elements = %d", sum);
+
+    return 0;
+}
