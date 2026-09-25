ad5f78cbf74b2cc39e8d55661a20a370a575421f
veersingh14 <veersingh4178@gmail.com>
2026-09-25 17:21:22 +0000

    day042 q1

---
+++ day042/day042q1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+    int i, vowels = 0, consonants = 0;
+
+    printf("Enter a string: ");
+    scanf("%[^\n]", str);
+
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' ||
+            str[i] == 'o' || str[i] == 'u' ||
+            str[i] == 'A' || str[i] == 'E' || str[i] == 'I' ||
+            str[i] == 'O' || str[i] == 'U')
+        {
+            vowels++;
+        }
+        else if ((str[i] >= 'a' && str[i] <= 'z') ||
+                 (str[i] >= 'A' && str[i] <= 'Z'))
+        {
+            consonants++;
+        }
+    }
+
+    printf("Vowels = %d\n", vowels);
+    printf("Consonants = %d", consonants);
+
+    return 0;
+}
