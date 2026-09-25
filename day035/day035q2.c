c56fec1e167d988f5092010db33ceedb52e2ec55
veersingh14 <veersingh4178@gmail.com>
2026-09-25 16:33:44 +0000

    day035 q2

---
+++ day035/day035q2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[100], n, k, i, j, temp;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter array elements: ");
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    printf("Enter k: ");
+    scanf("%d", &k);
+
+    for (i = 0; i < k; i++)
+    {
+        temp = a[n - 1];
+
+        for (j = n - 1; j > 0; j--)
+        {
+            a[j] = a[j - 1];
+        }
+
+        a[0] = temp;
+    }
+
+    printf("Array after rotation: ");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
